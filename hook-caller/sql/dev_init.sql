a77b31f500ad6d1205356d6c71f21a125b67a129
Guuzzeji <5lh8dl440f@gmail.com>
2026-04-16 19:06:32 -0700

    db: adding webhooks_logs for end user to view

--- hook-caller/sql/dev_init.sql
+++ hook-caller/sql/dev_init.sql
@@ -9,15 +9,28 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS webhooks (
+    id          BIGINT PRIMARY KEY INCREMENT BY 1,
     user_id     UUID NOT NULL,
     hook_url    TEXT NOT NULL,
     sign_key    TEXT NOT NULL,
     is_fte      BOOLEAN NOT NULL DEFAULT TRUE,
     is_intern   BOOLEAN NOT NULL DEFAULT FALSE,
-    is_active   BOOLEAN NOT NULL DEFAULT TRUE,
+    is_active   BOOLEAN NOT NULL DEFAULT FALSE,
     is_markdown BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+CREATE TABLE IF NOT EXISTS webhooks_log (
+  webhook_id      BIGINT REFERENCES webhooks (id),
+  created_at      TIMESTAMPTZ DEFAULT now(),
+  success         BOOLEAN NOT NULL DEFAULT FALSE,
+  error_message   TEXT DEFAULT NULL,
+  status_code     INTEGER DEFAULT NULL
+  jobs_payload    TEXT DEFAULT NULL
+  is_test         BOOLEAN NOT NULL DEFAULT FALSE
+)
+
+CREATE INDEX IF NOT EXISTS idx_webhooks_log_webhook_id ON webhooks_log (webhook_id);
+CREATE INDEX IF NOT EXISTS idx_webhooks_user_id ON webhooks (user_id);
 CREATE INDEX IF NOT EXISTS idx_webhooks_is_active ON webhooks (is_active) WHERE is_active = TRUE;
 CREATE INDEX IF NOT EXISTS idx_user_github_id ON users (github_id);
 CREATE INDEX IF NOT EXISTS idk_webhooks_is_fte ON webhooks (is_fte) WHERE is_fte = TRUE;
