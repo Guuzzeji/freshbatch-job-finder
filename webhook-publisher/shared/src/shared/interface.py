0ba61eab236cf5ce750e5b4b6e82e954c8a95efe
Guuzzeji <5lh8dl440f@gmail.com>
2026-08-01 01:04:21 -0700

    chore(shared-py): copy over files for docker build

---
+++ webhook-publisher/shared/src/shared/interface.py
@@ -0,0 +1,117 @@
+import json
+
+class JobInformation:
+    def __init__(self,
+                 company_name: str,
+                 title: str,
+                 date_posted: int,
+                 url: str,
+                 source: str,
+                 degrees: list[str],
+                 sponsorship: str,
+                 locations: list[str],
+                 is_fte: bool,
+                 is_intern: bool,
+                 category: str,
+                 is_test: bool = False) -> None:
+        self.is_test = is_test
+        self.is_fte = is_fte
+        self.is_intern = is_intern
+        self.company_name = company_name
+        self.title = title
+        self.date_posted = date_posted
+        self.url = url
+        self.source = source
+        self.degrees = degrees
+        self.sponsorship = sponsorship
+        self.locations = locations
+        self.category = category
+
+    def to_json(self) -> dict:
+        return {
+            "is_test": self.is_test,
+            "is_fte": self.is_fte,
+            "is_intern": self.is_intern,
+            "company_name": self.company_name,
+            "title": self.title,
+            "date_posted": self.date_posted,
+            "url": self.url,
+            "source": self.source,
+            "degrees": self.degrees,
+            "sponsorship": self.sponsorship,
+            "locations": self.locations,
+            "category": self.category
+        }
+    
+    def __str__(self) -> str:
+        return str(self.to_json())
+    
+    def dump(self) -> str:
+        return json.dumps(self.to_json())
+    
+    @staticmethod
+    def from_string(json_str_or_dict: str | dict) -> JobInformation:
+        if isinstance(json_str_or_dict, dict):
+            json_obj = json_str_or_dict
+        else:
+            json_obj = json.loads(json_str_or_dict)
+        return JobInformation(
+            is_test=json_obj["is_test"],
+            is_fte=json_obj["is_fte"],
+            is_intern=json_obj["is_intern"],
+            company_name=json_obj["company_name"],
+            title=json_obj["title"],
+            date_posted=int(json_obj["date_posted"]),
+            url=json_obj["url"],
+            source=json_obj["source"],
+            degrees=json_obj["degrees"],
+            sponsorship=json_obj["sponsorship"],
+            locations=json_obj["locations"],
+            category=json_obj["category"]
+        ) 
+
+    @staticmethod
+    def from_string_list(json_str_list: str | list) -> list[JobInformation]:
+        if isinstance(json_str_list, list):
+            json_obj = json_str_list
+        else:
+            json_obj = json.loads(json_str_list)
+        result = []
+        for job in json_obj:
+            result.append(JobInformation.from_string(job))
+        return result
+
+class HookerInformation:
+    def __init__(self,
+                 webhook_id: int,
+                 sign_key: str,
+                 hook_url: str) -> None:
+        self.webhook_id = webhook_id
+        self.sign_key = sign_key
+        self.hook_url = hook_url
+
+    def to_json(self) -> dict:
+        return {
+            "webhook_id": self.webhook_id,
+            "sign_key": self.sign_key,
+            "hook_url": self.hook_url
+        }
+    
+    def __str__(self) -> str:
+        return str(self.to_json())
+    
+    def dump(self) -> str:
+        return json.dumps(self.to_json())
+
+    @staticmethod
+    def from_string(json_str: str) -> 'HookerInformation':
+        json_obj = json.loads(json_str)
+        try:
+            webhook_id = int(json_obj["webhook_id"])
+        except (KeyError, TypeError, ValueError):
+            webhook_id = -1
+        return HookerInformation(
+            webhook_id=webhook_id,
+            sign_key=json_obj["sign_key"],
+            hook_url=json_obj["hook_url"]
+        )
